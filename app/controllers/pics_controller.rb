23e1cb6c4c6d0a6f8b5cc54b79779c524f417100
Denise Thornberry <Denise@Denises-MacBook-Pro-2.local>
2014-02-08 17:10:57 -0500

    adding entries and bouncing to pic page

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,7 +1,24 @@
 class PicsController < ApplicationController
 
-def index
+	def index
 		@pics = Pic.all
-end
+	end
+
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		Pic.create( pics_params )
+		redirect_to pics_path
+	end
+
+
+	private
+
+	def pics_params
+		params.require(:pic).permit(:hour, :emotion, :learned)
+
+	end
 
 end
