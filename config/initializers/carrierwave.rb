a6132203b7c3743a2143478e3f03ef9d1d7bb09d
Denise Thornberry <Denise@Denises-MacBook-Pro-2.local>
2014-02-09 12:20:47 -0500

    linking AWS bucket to image entry

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,16 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',                        
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],                     
+      :aws_secret_access_key  => ENV['AWS_SECRET_KEY']                        
+    }
+
+    config.fog_directory  = ENV['AWS_BUCKET']  
+
+    else
+      config.storage = :file
+    end                  
+  
+end
